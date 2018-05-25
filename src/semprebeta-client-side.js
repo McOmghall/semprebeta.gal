cd34bc0b354a166eca6f436698550063b708f703
McOmghall <pmgberm@gmail.com>
2018-05-25 08:38:20 +0200

    Small test

--- src/semprebeta-client-side.js
+++ src/semprebeta-client-side.js
@@ -1,11 +1,13 @@
 const mocha = require('mocha')
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('GA')
   window.ga = window.ga || function () { (window.ga.q = window.ga.q || []).push(arguments) }
   window.ga.l = new Date().getTime()
   window.ga('create', process.env.GOOGLE_ANALYTICS_ID, 'auto')
   window.ga('send', 'pageview')
 } else if (process.env.NODE_ENV === 'test') {
+  console.log('MO')
   require('./semprebeta-client-side.test')
   mocha.run()
 }
