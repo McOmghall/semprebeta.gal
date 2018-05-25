389bd31f9834432b8ce8cfade5d72800c0ba113b
McOmghall <pmgberm@gmail.com>
2018-05-25 10:40:49 +0200

    Repaired GA script

--- src/semprebeta-client-side.js
+++ src/semprebeta-client-side.js
@@ -3,7 +3,7 @@ const mocha = require('mocha')
 if (process.env.NODE_ENV === 'production') {
   console.log('GA')
   const gaHref = 'https://www.google-analytics.com/analytics.js'
-  window.ga = window.ga || function () { (window.ga.q = window.ga.q || []).push(window, document, 'script', gaHref, 'ga') }
+  window.ga = window.ga || function(){ (ga.q = ga.q || []).push(arguments) }
   window.ga.l = new Date().getTime()
   const insert = document.createElement('script')
   insert.async = 1
@@ -14,8 +14,10 @@ if (process.env.NODE_ENV === 'production') {
 
   window.ga('create', process.env.GOOGLE_ANALYTICS_ID, 'auto')
   window.ga('send', 'pageview')
+  console.log('GA-END')
 } else if (process.env.NODE_ENV === 'test') {
   console.log('MO')
   require('./semprebeta-client-side.test')
   mocha.run()
+  console.log('MO-END')
 }
